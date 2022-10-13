c945f0f380f5082dce7340f1f32f57b552eb5b12
David Castagneto A <76570078+davidcastagnetoa@users.noreply.github.com>
2022-10-13 23:22:58 +0000

    app.css

--- personal-portfolio-tutorial/src/components/NavBar.js
+++ personal-portfolio-tutorial/src/components/NavBar.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 export const navBar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -27,7 +31,7 @@ export const navBar = () => {
     <Navbar bg="light" expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="Logo" />
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toogler-icon"></span>
@@ -65,9 +69,9 @@ export const navBar = () => {
           <span className="navbar-text">
             <div className="social-icon">
               <a href="#">
-                <img src={""} alt="" />
-                <img src={""} alt="" />
-                <img src={""} alt="" />
+                <img src={navIcon1} alt="" />
+                <img src={navIcon2} alt="" />
+                <img src={navIcon3} alt="" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}>
