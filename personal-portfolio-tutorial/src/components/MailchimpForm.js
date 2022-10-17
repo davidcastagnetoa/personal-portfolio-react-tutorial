d6f4d6c171d18c57e632a528ef82e1fb3c5f9b96
David Castagneto A <davidcastagnetoa@gmail.com>
2022-10-17 02:26:44 +0200

    server.js

--- personal-portfolio-tutorial/src/components/MailchimpForm.js
+++ personal-portfolio-tutorial/src/components/MailchimpForm.js
@@ -2,7 +2,8 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { Newsletter } from "./Newsletter";
 
 export const MailchimpForm = () => {
-  const postUrl = `https://gmail.us8.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+    // Embedded forms. from Audience in your mailchimp account
+    const postUrl = `https://gmail.us8.list-manage.com/subscribe/post?u=801d74e75451700de636df2a6&amp;id=98657f85e9&amp;f_id=00f25fe0f0`;
 
   return (
     <>
